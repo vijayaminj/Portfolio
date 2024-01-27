6dee8b4bcb55fd3126344a3a908b31079f2bb878
Vijaya Minj <vijminj@gmail.com>
2024-01-27 21:48:09 +0530

    updated

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,6 @@ let cross=document.querySelector(".cross");
 let list=document.querySelector(".list");
 
 cross.addEventListener("click",()=>{
-    list.classList.toggle("open");
-    
+    list.classList.toggle("open");   
     
 })
